f38c17b0b4d14621f135377d8cb1b0a2a54cc276
Franck-Nein <franco600@live.com.ar>
2020-11-14 08:10:56 -0300

    update

---
+++ 2/watch/test.py
@@ -0,0 +1,19 @@
+import sys
+import urllib, os
+with open("archive.txt", "r") as mylist:
+ with open("temp.txt", "r") as temp:
+  for line in mylist:
+   vidid = line.strip()
+   print(vidid)
+#   with open('temp.txt', 'w') as f:
+   os.system("grep -h '" + vidid + "' emb.txt | head -1 > temp.txt") 
+   for temp in open('temp.txt'):
+    url = temp.strip()
+    replace = url.split('/')[4]
+    vidid = vidid.replace("[","")
+    vidid = vidid.replace("]","")
+    vidid = vidid.replace("-","-- -")
+   os.system("sed 's,1AMJ8mJ1BrheeQq," + replace + ",g' -- " + vidid + "/index.html")
+
+#  with open('temp.txt', 'w') as f:
+#   sys.stdout = f
