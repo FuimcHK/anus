53218be4ca1240a126b10e047101934e593b0134
Franck-Nein <franco600@live.com.ar>
2020-11-14 08:23:25 -0300

    update

--- 2/watch/test.py
+++ 2/watch/test.py
@@ -4,7 +4,7 @@ with open("archive.txt", "r") as mylist:
  with open("temp.txt", "r") as temp:
   for line in mylist:
    vidid = line.strip()
-   print(vidid)
+#   print(vidid)
 #   with open('temp.txt', 'w') as f:
    os.system("grep -h '" + vidid + "' emb.txt | head -1 > temp.txt") 
    for temp in open('temp.txt'):
@@ -12,8 +12,7 @@ with open("archive.txt", "r") as mylist:
     replace = url.split('/')[4]
     vidid = vidid.replace("[","")
     vidid = vidid.replace("]","")
-    vidid = vidid.replace("-","-- -")
-   os.system("sed 's,1AMJ8mJ1BrheeQq," + replace + ",g' -- " + vidid + "/index.html")
+   os.system("sed -i 's,1AMJ8mJ1BrheeQq," + replace + ",g' -- " + vidid + "/index.html")
 
 #  with open('temp.txt', 'w') as f:
 #   sys.stdout = f
